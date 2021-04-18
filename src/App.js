67bc98c0d2622eecc369a883f334fc0d953f7a4d
YanaGuzienko <69dusha69@ukr.net>
2021-04-18 12:58:45 +0300

    меняет паддинг

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ const App = () => {
         likes={user.stats.likes}
       />
       <Statistics title='Upload stats' stats={statisticalData} />
-      <Statistics stats={statisticalData} />
+      {/* <Statistics stats={statisticalData} /> */}
       <FriendList friends={friends} />
       <TransactionHistory items={items} />
     </Container>
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -2,21 +2,19 @@ import PropTypes from 'prop-types';
 import scss from './Statistics.module.scss';
 import randColor from './Statistics';
 
-const Statistics = ({ title, stats }) => {
-  return (
-    <section className={scss.statistics}>
-      {title && <h2 className={scss.title}> {title}</h2>}
-      <ul className={scss.list}>
-        {stats.map(({ id, label, percentage }) => (
-          <li className={scss.item} key={id} style={{ backgroundColor: randColor() }}>
-            <span className={scss.label}>{label}</span>
-            <span className={scss.percentage}>{percentage}%</span>
-          </li>
-        ))}
-      </ul>
-    </section>
-  );
-};
+const Statistics = ({ title, stats }) => (
+  <section className={scss.statistics}>
+    {title && <h2 className={scss.title}> {title}</h2>}
+    <ul className={scss.list}>
+      {stats.map(({ id, label, percentage }) => (
+        <li className={scss.item} key={id} style={{ backgroundColor: randColor() }}>
+          <span className={scss.label}>{label}</span>
+          <span className={scss.percentage}>{percentage}%</span>
+        </li>
+      ))}
+    </ul>
+  </section>
+);
 
 Statistics.protoTypes = {
   title: PropTypes.string,
